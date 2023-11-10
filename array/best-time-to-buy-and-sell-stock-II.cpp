952f887afb352d06e3896aede41bee833f955480
mahbub-hasan <mahbubhasanmht@gmail.com>
2023-11-11 00:16:21 +0600

    Problem-7 solution added

--- array/best-time-to-buy-and-sell-stock-II.cpp
+++ array/best-time-to-buy-and-sell-stock-II.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0, n = prices.size();
+        int ans = 0;
+        int n = prices.size();
         for(int i=0;i<n-1;i++){
             if(prices[i]<prices[i+1]){
                 ans+=prices[i+1]-prices[i];
