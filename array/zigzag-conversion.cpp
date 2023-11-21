684e5131dc65320beba0693de3e6ddfe1a580b2f
mahbub-hasan <mahbubhasanmht@gmail.com>
2023-11-21 23:06:07 +0600

    problem 20 & 21 added

---
+++ array/zigzag-conversion.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int N = numRows;
+        int n = s.size();
+        vector<int>tem;
+        for(int i=0;i<N;i++)tem.push_back(i);
+        for(int i=N-2;i>0;i--)tem.push_back(i);
+        map<int,string>mp;
+        for(int i=0;i<N;i++)mp[i] = "";
+        for(int i=0;i<n;i++){
+            int ind = tem[i%tem.size()];
+            mp[ind]+=s[i];
+        }
+        string fin = "";
+        for(auto it:mp){
+            fin+=it.second;
+        }
+        return fin;
+    }
+};
